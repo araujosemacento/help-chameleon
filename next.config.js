dedfac5a0195207382c1674077f4632066698c6c
G. Melo <araujosemacento@alu.ufc.br>
2023-10-29 03:47:41 -0300

    PWA config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const withPWA = require('next-pwa');
+const plugin = require('tailwindcss');
+
+const nextConfig = {
+    ...withPWA({
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+        //disable: process.env.NODE_ENV === 'development',
+    })
+}
 
 module.exports = nextConfig
