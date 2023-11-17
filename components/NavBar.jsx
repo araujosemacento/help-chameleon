ff9bf357365249b1f3378dfe4128de4c9348cf83
G. Melo <araujosemacento@alu.ufc.br>
2023-11-17 11:32:40 -0300

    Nav Title

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function NavBar() {
   return (
     <header className={`flex flex-row p-2 h-16 w-full bg-[#4b1eae]`}>
-      <div className={`flex flex-row items-center gap-4 mx-2`}>
+      <div className={`flex relative flex-row w-full items-center gap-4 mx-2`}>
         <Image
           src="/hameleon.png"
           alt="logo"
@@ -13,12 +13,14 @@ export default function NavBar() {
           className={`rounded-full w-[12vw] h-[12vw] sm:w-16 sm:h-16 drop-shadow-lg`}
         />
 
-        <h1
-          className={`text-[#e6e6e6]`}
-          style={{ textShadow: "1px 3px 5px #13072c" }}
-        >
-          Programação II
-        </h1>
+        <div className="flex flex-row absolute w-full place-content-center md:place-content-start md:px-20">
+          <h1
+            className={`text-[#e6e6e6]`}
+            style={{ textShadow: "1px 3px 5px #13072c" }}
+          >
+            Programação II
+          </h1>
+        </div>
       </div>
     </header>
   );
