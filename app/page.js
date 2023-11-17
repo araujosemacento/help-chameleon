372a469e8c39afc54062699a94895c16a55d5de1
G. Melo <araujosemacento@alu.ufc.br>
2023-11-17 11:11:49 -0300

    Resize Handling

--- app/page.js
+++ app/page.js
@@ -4,17 +4,28 @@ import * as React from "react";
 import NavBar from "@/components/NavBar";
 import { useRouter } from "next/navigation";
 import { Button } from "@mui/material";
+
 export default function Home() {
   const router = useRouter();
 
   const [isMobile, setIsMobile] = React.useState(false);
-  const [size, setSize] = React.useState(0);
   const [panel, setPanel] = React.useState(true);
+  const [dimensions, setDimensions] = React.useState([]);
 
   React.useEffect(() => {
-    setSize(window.innerWidth);
-    setIsMobile(size < 720);
-  }, [size]);
+    const handleResize = () => {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+      setIsMobile(dimensions.width < 720);
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    }
+  }, [dimensions.width]);
 
   return (
     <div className={`flex flex-col min-h-screen bg-background-100`}>
@@ -31,15 +42,15 @@ export default function Home() {
           }
         />
         <section
-          className={`flex absolute w-full md:w-[70%] h-[70%] md:h-full top-[65%] md:top-0 md:left-[65%] rounded-3xl bg-gradient-to-br from-primary-200 to-accent-400 z-20 transition-all duration-600 overflow-hidden shadow-[0_0_5px_0.25px_var(--secondary-600)]`}
+          className={`flex flex-col md:flex-row absolute w-full md:w-[70%] h-[70%] md:h-full top-[65%] md:top-0 md:left-[65%] rounded-3xl bg-gradient-to-br from-primary-200 to-accent-400 z-20 transition-all duration-600 overflow-hidden shadow-[0_0_5px_0.25px_var(--secondary-600)]`}
           style={
             isMobile
               ? { transform: `translateY(-${panel ? 0 : 143}%)` }
               : { transform: `translateX(-${panel ? 0 : 143}%)` }
           }
         >
-          <div className="h-full w-1/2 p-10"></div>
-          <div className="h-full w-1/2 p-10"></div>
+          <div className="flex flex-row md:flex-col justify-center items-center text-center gap-4 text-text-900 h-1/2 md:h-full w-full md:w-1/2 p-6"></div>
+          <div className="flex flex-row md:flex-col justify-center items-center text-center gap-4 text-text-900 h-1/2 md:h-full w-full md:w-1/2 p-6"></div>
         </section>
         <section
           className={`flex absolute w-full h-full top-[100%] md:top-0 md:left-[100%] z-10 transition-all duration-600`}
@@ -49,6 +60,7 @@ export default function Home() {
               : { transform: `translateX(-${panel ? 0 : 100}%)` }
           }
         />
+        <Button className="absolute left-[50%] top-[50%] z-50" onClick={() => { setPanel(!panel) }}>Trocar</Button>
       </main>
     </div>
   );
