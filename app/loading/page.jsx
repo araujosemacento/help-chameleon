4146631adc354555960bdd282c41e18cdf3dac87
G. Melo <araujosemacento@alu.ufc.br>
2023-11-29 13:13:47 -0300

    Typo

--- app/loading/page.jsx
+++ app/loading/page.jsx
@@ -1,8 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { getUserDataAsObject } from "@/app/firebase";
 
 export default function Load() {
-  React.useEffect(() => {});
-
   return <div />;
 }
